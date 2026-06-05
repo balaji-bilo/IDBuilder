78bfe537fc1602e31586f98297fd26f80d6e4f40
balaji <balajig.saitech@gmail.com>
2026-06-05 16:46:21 +0530

    first commit

--- src/middleware/uploadPhoto.js
+++ src/middleware/uploadPhoto.js
@@ -30,7 +30,7 @@ const fileFilter = (req, file, cb) => {
 const uploadPhoto = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 },
+  limits: { fileSize: 2 * 1024 * 1024 },
 });
 
 module.exports = uploadPhoto;
